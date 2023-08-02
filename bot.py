94470c9c4fed83d3163b015e5340e67172399a94
Yusubov Shahlar <yusubvss@gmail.com>
2023-08-02 19:41:48 +0300

    first commit

---
+++ bot.py
@@ -0,0 +1,34 @@
+import asyncio
+from distutils.command.config import config
+import logging, config
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+# Объект бота
+bot = Bot(token=config.BOT_TOKEN)
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+@dp.message(Command("photo"))
+async def cmd_start(message: types.Message):
+    await message.answer("Give mi your photo!")
+
+@dp.message(Command("dice"))
+async def cmd_dice(message: types.Message):
+    await message.answer_dice(emoji="🎲")
+
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
